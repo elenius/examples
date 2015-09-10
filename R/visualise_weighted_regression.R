97f78628ee739262f2ba0b6ec823d1d6ae749a09
Mikael Elenius <mikael.elenius@pensionsmyndigheten.se>
2015-09-10 10:50:14 +0200

    ggplot -> ggplot2

--- R/visualise_weighted_regression.R
+++ R/visualise_weighted_regression.R
@@ -1,14 +1,13 @@
 # example to visualise weighted regression
 library(dplyr)
-library(ggplot)
-
+library(ggplot2)
 
 set.seed(123)
 d1 <- data_frame(x= rnorm(100, 100, 5),
                  y = 3 + 2*x + rnorm(100, 0, 10))
 
 # adding weights on deviating observations
-d1 <- 
+d1 <-
     d1 %>%
     mutate(weights = ifelse((x < 100 & y> 200) | (x>100 & y < 200), 25, 1),
            max_weight = max(weights)
@@ -16,10 +15,10 @@ d1 <-
 
 lm(y~x, data=d1, weights=weights)
 
-ggplot(d1) + aes(x=x, y=y) + 
-    scale_size_area() + 
-    geom_point(aes(size=sqrt(weights))) + # , alpha=sqrt(weights)/sqrt(max_weight))) + 
-    theme_minimal() + 
-    theme(legend.position="none") + 
-    geom_smooth(method="lm",  aes(weight=weights), colour="darkgreen") + 
+ggplot(d1) + aes(x=x, y=y) +
+    scale_size_area() +
+    geom_point(aes(size=sqrt(weights))) + # , alpha=sqrt(weights)/sqrt(max_weight))) +
+    theme_minimal() +
+    theme(legend.position="none") +
+    geom_smooth(method="lm",  aes(weight=weights), colour="darkgreen") +
     geom_smooth(method="lm")
