a9a03963998e74f1630096d7dd6dbdbe2afb8182
Mikael Elenius <mikael.elenius@pensionsmyndigheten.se>
2015-09-17 07:55:12 +0200

    box inside violin

--- R/boxplot_jitter.R
+++ R/boxplot_jitter.R
@@ -7,7 +7,7 @@ library(ggplot2)
 n <- 1000
 set.seed(123)
 d1 <-
-  data_frame(A = rnorm(n, -3, 3), B = rnorm(n, 5, 1), C = rexp(n, 1)) %>%
+  data_frame(A = rnorm(n, -1, 2), B = rnorm(n, 5, 1), C = rexp(n, 1)) %>%
   gather(group, value)
 
 # boxplot
@@ -17,8 +17,12 @@ ggplot(d1) + aes(x=group, y=value, fill=group) +
   theme_minimal()
 
 # extra violinplot
-ggplot(d1) + aes(x=group, y=value, fill=group) +
-  geom_jitter(position = position_jitter(width = .2), alpha=0.2, size=0.8) +
+ggplot(d1) +
+  aes(x=group, y=value, fill=group) +
+  geom_jitter(position = position_jitter(width = 0.05, height=0.01), alpha=0.1, size=1) +
   geom_violin(alpha=0.1) +
-  geom_boxplot(outlier.size=0, alpha=0.1) +
-  theme_minimal()
+  geom_boxplot(outlier.size=0, alpha=0.1, width=0.2) +
+  theme_minimal() + xlab("") +
+  coord_flip() +
+  theme(legend.position="none")
+
